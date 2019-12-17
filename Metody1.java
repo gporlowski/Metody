912d333267e0e44a24042ec2f4c3567c642d72bf
gporlowski <58628126+gporlowski@users.noreply.github.com>
2019-12-17 15:27:14 +0100

    Metody 1

---
+++ Metody1.java
@@ -0,0 +1,9 @@
+public class Znaki9 {
+  public static void main(String[] args) {
+    int firstInteger = 15, secondInteger = 3;
+    System.out.println(firstInteger + " x " + secondInteger + " = " + multiply(firstInteger, secondInteger));
+  }
+  public static int multiply(int firstNumber, int secondNumber) {
+    return firstNumber * secondNumber;
+  }
+}
